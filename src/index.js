727edd71b9d2d97cf2cec29d540df4dc79762e7c
Michelon Souza <michelon.souza@hotmail.com>
2020-02-11 21:33:03 -0300

    implements splash-screen ios

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import 'react-native-gesture-handler';
+import SplashScreen from 'react-native-splash-screen';
 
+import { colors } from './styles';
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <StatusBar barStyle="light-content" backgroundColor={colors.white} />
       <Routes />
     </>
   );
